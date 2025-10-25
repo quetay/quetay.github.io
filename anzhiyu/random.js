340e849afbca95783ea4d4e3c4df764d0cdf566c
rookie <2331975780@qq.com>
2025-10-25 13:08:23 +0800

    Site updated: 2025-10-25 13:08:23

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/22/hello-world/"];function toRandomPost(){
+var posts=["2025/10/24/LoserTree/","2025/10/22/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
